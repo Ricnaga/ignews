ca0b9428e2a1debba3a4d3f2b9ec1eecc3076642
Ricnaga <47759922+Ricnaga@users.noreply.github.com>
2021-03-25 01:03:15 -0300

    set configuration of github access with oauth | fauna queries

--- src/components/SignInButton/index.tsx
+++ src/components/SignInButton/index.tsx
@@ -1,24 +1,29 @@
 import { FaGithub } from 'react-icons/fa'
 import { FiX } from 'react-icons/fi'
+import { signIn, signOut, useSession } from 'next-auth/client'
 import styles from './styles.module.scss'
 
 export function SignInButton() {
-    const isUserLoggedIn = true
-    return isUserLoggedIn ? (
-        <button type="button"
-        className={styles.SignInButton}>
-            <FaGithub color="#04d361"/>
-            Ricardo
 
-            <FiX 
-            color="#737380"
-            className={styles.closeIcon}
+    const [session] = useSession()
+
+    return session ? (
+        <button type="button"
+            className={styles.SignInButton}
+            onClick={() => signOut()}
+        >
+            <FaGithub color="#04d361" />
+            {session.user.name}
+            <FiX
+                color="#737380"
+                className={styles.closeIcon}
             />
         </button>
-    ) :(
+    ) : (
         <button type="button"
-        className={styles.SignInButton}>
-            <FaGithub color="#eba417"/>
+            className={styles.SignInButton}
+            onClick={() => signIn('github')}>
+            <FaGithub color="#eba417" />
             Sign In with GitHub
         </button>
     )

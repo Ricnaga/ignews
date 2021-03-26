95b98b7a5557d4edd5726064662c21529c525831
Ricnaga <47759922+Ricnaga@users.noreply.github.com>
2021-03-25 23:01:55 -0300

    set page posts and contents call to prismic api

--- src/components/SubscribeButton/index.tsx
+++ src/components/SubscribeButton/index.tsx
@@ -1,4 +1,5 @@
 import { useSession, signIn } from 'next-auth/client'
+import { useRouter } from 'next/router';
 import { api } from '../../services/api';
 import { getStripeJs } from '../../services/stripe-js';
 import styles from './styles.module.scss'
@@ -9,12 +10,20 @@ interface SubscribeButtonProps {
 
 export function SubscribeButton({ priceId }: SubscribeButtonProps) {
     const [session] = useSession()
+    const router = useRouter()
 
     async function handleSubscribe() {
         if (!session) {
             signIn('github')
             return;
         }
+
+        if (session.activeSubscription) {
+            router.push('/posts')
+            return;
+        }
+
+
 
         try {
             const response = await api.post('/subscribe')
@@ -23,8 +32,8 @@ export function SubscribeButton({ priceId }: SubscribeButtonProps) {
 
             const stripe = await getStripeJs()
 
-            await stripe.redirectToCheckout({sessionId})
-            
+            await stripe.redirectToCheckout({ sessionId })
+
         } catch (error) {
             alert(error.message)
         }

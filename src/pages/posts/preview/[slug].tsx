ee1d8738ef93f98839a5edba1fe51cc0a8b1c432
Ricnaga <47759922+Ricnaga@users.noreply.github.com>
2021-03-26 12:05:39 -0300

    rendering posts content for non-subscribers

--- src/pages/posts/preview/[slug].tsx
+++ src/pages/posts/preview/[slug].tsx
@@ -1,4 +1,4 @@
-import { GetStaticProps } from "next"
+import { GetStaticPaths, GetStaticProps } from "next"
 import { useSession } from "next-auth/client"
 import Head from "next/head"
 import Link from "next/link"
@@ -23,9 +23,9 @@ export default function PostPreview({ post }: PostPreviewProps) {
 
     useEffect(() => {
         if (session?.activeSubscription) {
-            router.push(`/posts/${post.slug}`)            
+            router.push(`/posts/${post.slug}`)
         }
-     }, [session])
+    }, [session])
     return (
         <>
             <Head>
@@ -57,7 +57,7 @@ export default function PostPreview({ post }: PostPreviewProps) {
     )
 }
 
-export const getStaticPaths = () => {
+export const getStaticPaths: GetStaticPaths = async () => {
     return {
         paths: [],
         fallback: 'blocking'
@@ -83,6 +83,7 @@ export const getStaticProps: GetStaticProps = async ({ params }) => {
     return {
         props: {
             post
-        }
+        },
+        redirect: 60 * 30 //30 minutes
     }
 }

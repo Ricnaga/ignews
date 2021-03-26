8f025aa0b41f8c9340e7e42459171379275a46f9
Ricnaga <47759922+Ricnaga@users.noreply.github.com>
2021-03-26 11:42:50 -0300

    create post preview

--- src/pages/posts/[slug].tsx
+++ src/pages/posts/[slug].tsx
@@ -37,7 +37,7 @@ export const getServerSideProps: GetServerSideProps = async ({ req, params }) =>
     const session = await getSession({ req })
     const { slug } = params
 
-    if (!session.activeSubscription) {
+    if (!session?.activeSubscription) {
         return {
             redirect: {
                 destination: '/',
@@ -46,7 +46,7 @@ export const getServerSideProps: GetServerSideProps = async ({ req, params }) =>
         }
     }
 
-    const prismic = getPrismicClient()
+    const prismic = getPrismicClient(req)
     const response = await prismic.getByUID('publication', String(slug), {})
 
     const post = {
